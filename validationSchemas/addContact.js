6c5a1195c05c3f12e797543b781c82a15d5b47b1
Anastasiia <guba.anastaysha@gmail.com>
2023-01-20 13:32:55 +0200

    added validation methods

--- validationSchemas/addContact.js
+++ validationSchemas/addContact.js
@@ -1,9 +1,15 @@
 const Joi = require("joi");
 
 const addContact = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().min(2).max(30).required(),
   email: Joi.string().required(),
-  phone: Joi.string().required(),
+  phone: Joi.string()
+    .pattern(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/
+    )
+    .min(2)
+    .max(30)
+    .required(),
 });
 
 module.exports = addContact;
--- validationSchemas/updateContact.js
+++ validationSchemas/updateContact.js
@@ -1,9 +1,14 @@
 const Joi = require("joi");
 
 const updateContact = Joi.object({
-  name: Joi.string(),
+  name: Joi.string().min(2).max(30),
   email: Joi.string(),
-  phone: Joi.string(),
+  phone: Joi.string()
+    .pattern(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/
+    )
+    .min(2)
+    .max(30),
 });
 
 module.exports = updateContact;

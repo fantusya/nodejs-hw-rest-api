527c4f439d07be72be1771a7fa8a3a25ec10511a
Anastasiia <guba.anastaysha@gmail.com>
2023-02-14 14:37:45 +0200

    added for a short import

---
+++ controllers/auth/index.js
@@ -0,0 +1,13 @@
+const signup = require("./signup");
+const login = require("./login");
+const getCurrent = require("./getCurrent");
+const updateSubscription = require("./updateSubscription");
+const logout = require("./logout");
+
+module.exports = {
+  signup,
+  login,
+  getCurrent,
+  logout,
+  updateSubscription,
+};

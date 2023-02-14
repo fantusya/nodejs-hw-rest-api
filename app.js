e097460efaa961766c5d9eb6982c9d55775d2ddf
Anastasiia <guba.anastaysha@gmail.com>
2023-02-14 14:36:33 +0200

    added user router

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
+const authRouter = require("./routes/api/auth");
 const contactsRouter = require("./routes/api/contacts");
 
 dotenv.config();
@@ -14,6 +16,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {

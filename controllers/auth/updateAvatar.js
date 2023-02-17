57630216761784823e745dbd3ba29added47f378
Anastasiia <guba.anastaysha@gmail.com>
2023-02-17 22:32:46 +0200

    added a controller to update an avatar

---
+++ controllers/auth/updateAvatar.js
@@ -0,0 +1,38 @@
+const { User } = require("../../models");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
+
+// const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+const avatarsDir = path.resolve("public/avatars");
+
+const updateAvatar = async (req, res) => {
+  const { path: tmpUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+  const imageName = `${id}_${originalname}`;
+
+  try {
+    const resultUpload = path.join(avatarsDir, imageName);
+
+    const image = await Jimp.read(tmpUpload);
+    await image
+      .autocrop()
+      .cover(
+        250,
+        250,
+        Jimp.HORIZONTAL_ALIGN_CENTER || Jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .writeAsync(tmpUpload);
+
+    await fs.rename(tmpUpload, resultUpload);
+
+    const avatarURL = path.join("public", "avatars", imageName);
+    await User.findByIdAndUpdate(id, { avatarURL }, { new: true });
+    res.json({ avatarURL });
+  } catch (error) {
+    await fs.unlink(tmpUpload);
+    throw error;
+  }
+};
+
+module.exports = updateAvatar;

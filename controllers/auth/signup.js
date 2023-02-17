15cd352dbb84f766daa7a3662c93eff7d4dd8119
Anastasiia <guba.anastaysha@gmail.com>
2023-02-17 22:35:36 +0200

    added autogeneration of avatar when signing up

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,23 +1,21 @@
 const { Conflict } = require("http-errors");
-// const bcrypt = require("bcrypt");
+const gravatar = require("gravatar");
 const { User } = require("../../models");
 
 const signup = async (req, res) => {
   const { password, email, subscription = "starter" } = req.body;
-  console.log(subscription);
+
   const user = await User.findOne({ email });
   if (user) {
     throw new Conflict("Email in use");
   }
 
-  const newUser = new User({ email, subscription });
+  const avatarURL = gravatar.url(email, { s: "250" });
+  const newUser = new User({ email, subscription, avatarURL });
   newUser.setPassword(password);
   newUser.save();
 
-  // const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  // const result = await User.create({ password: hashPassword, email, subscription,});
-
-  res.status(201).json({ user: { email, subscription } });
+  res.status(201).json({ user: { email, subscription, avatarURL } });
 };
 
 module.exports = signup;

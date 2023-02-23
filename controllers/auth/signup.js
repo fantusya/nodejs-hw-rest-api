9d3ffe9927d8c65f74658f91484f3fb99fc0423e
Anastasiia <guba.anastaysha@gmail.com>
2023-02-23 12:40:15 +0200

    added creating of verification token when signup

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,6 +1,9 @@
 const { Conflict } = require("http-errors");
 const gravatar = require("gravatar");
+const { v4 } = require("uuid");
+
 const { User } = require("../../models");
+const { sendEmail } = require("../../helpers");
 
 const signup = async (req, res) => {
   const { password, email, subscription = "starter" } = req.body;
@@ -10,12 +13,28 @@ const signup = async (req, res) => {
     throw new Conflict("Email in use");
   }
 
+  const verificationToken = v4();
   const avatarURL = gravatar.url(email, { s: "250" });
-  const newUser = new User({ email, subscription, avatarURL });
-  newUser.setPassword(password);
-  newUser.save();
 
-  res.status(201).json({ user: { email, subscription, avatarURL } });
+  const newUser = new User({
+    email,
+    subscription,
+    avatarURL,
+    verificationToken,
+  });
+  newUser.setPassword(password);
+  await newUser.save();
+
+  const mailBody = {
+    to: email,
+    subject: "Confirm Your Email",
+    html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Click here to confirm your email.</a>`,
+  };
+  await sendEmail(mailBody);
+
+  res
+    .status(201)
+    .json({ user: { email, subscription, avatarURL, verificationToken } });
 };
 
 module.exports = signup;

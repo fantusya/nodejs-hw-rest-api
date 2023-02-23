1ffc23f18539fe222ef516eb7d4f4e4fe57f2781
Anastasiia <guba.anastaysha@gmail.com>
2023-02-23 12:34:45 +0200

    updated

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -4,6 +4,8 @@ const getCurrent = require("./getCurrent");
 const updateSubscription = require("./updateSubscription");
 const logout = require("./logout");
 const updateAvatar = require("./updateAvatar");
+const verifyEmail = require("./verifyEmail");
+const resendVerifyEmail = require("./resendVerifyEmail");
 
 module.exports = {
   signup,
@@ -12,4 +14,6 @@ module.exports = {
   logout,
   updateSubscription,
   updateAvatar,
+  verifyEmail,
+  resendVerifyEmail,
 };

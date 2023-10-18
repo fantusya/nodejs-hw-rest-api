7236ac132a354b3330a4f3149cea7c0e7299227f
Anastasiia <guba.anastaysha@gmail.com>
2023-10-18 10:51:15 +0300

    updated

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 3030 } = process.env;
+
+mongoose.set("strictQuery", false);
 
 mongoose
   .connect(DB_HOST)
